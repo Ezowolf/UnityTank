dda5967e652ca53b77a532a03ad0ec6b2f371447
Ezowolf <wiggler@live.nl>
2015-01-22 10:36:59 +0100

    Added CS scripts
    unity tank scripts

---
+++ Die.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Die : MonoBehaviour {
+	public GameObject explosionPreFab;
+	public int lives = 6;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnCollisionEnter(Collision coll)
+	{
+		if(coll.gameObject.name == "Bullet(Clone)")
+		{
+			lives--;
+			if(lives <= 0)
+			{
+				Instantiate(explosionPreFab, transform.position, transform.rotation);
+				Destroy(this.gameObject);
+			}
+		}
+	}
+}
---
+++ healthBar.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class healthBar : MonoBehaviour
+{
+	
+	GUIStyle style = new GUIStyle();
+	Texture2D texture;
+	public int healthWidth;
+	Color redColor = Color.red;
+	Color greenColor = Color.green;
+	Color yellowColor = Color.yellow;
+	public int curHealth = 100;
+	
+	void Start()
+	{
+		texture = new Texture2D(1, 1);
+		texture.SetPixel(1, 1, greenColor);
+	}
+	
+	private void Update()
+	{
+		
+		if (curHealth > 5)
+		{
+			texture.SetPixel(1, 1, greenColor);
+		}
+		
+		if (curHealth < 7)
+		{
+			texture.SetPixel(1, 1, yellowColor);
+		}
+		if (curHealth < 4)
+		{
+			texture.SetPixel(1, 1, redColor);
+		}
+	}
+	
+	public void OnGUI()
+	{
+		
+		texture.Apply();
+		
+		style.normal.background = texture;
+		GUI.Box(new Rect(0, 0, healthWidth, 20), new GUIContent(""), style);
+	}
+	void OnCollisionEnter(Collision coll)
+	{
+		if (coll.gameObject.name == "Bullet(Clone)")
+		{
+			curHealth--;
+			healthWidth -= 200/12;
+			if (curHealth <= 0)
+			{
+				Destroy(this.gameObject);
+			}
+		}
+	}
+}
